bb0bbf4f9a45efb349b7d9cebeaa506456305ab8
Student <Student@NCBE2L308S20>
2024-09-05 10:18:56 +0530

    Add project files.

---
+++ ARRAY/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyArray {
+    class ArrayProgarm
+    {
+        //sorting array in c#
+       static void Main(string[] args)
+        {
+            int[] Num = {2,4,1,5,6,0 };
+            Array.Sort(Num);
+            foreach (int i in Num)
+            {   
+                Console.WriteLine(i);
+            }
+        }
+    }
+
+}
+
+
